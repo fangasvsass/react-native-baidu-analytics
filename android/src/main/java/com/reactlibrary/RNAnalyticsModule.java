4a153f115149a48d22613657d2bba5415c584f9f
blake <blake.fang@letote.cn>
2018-03-29 16:19:16 +0800

    store the channel to sp

--- android/src/main/java/com/reactlibrary/RNAnalyticsModule.java
+++ android/src/main/java/com/reactlibrary/RNAnalyticsModule.java
@@ -8,114 +8,142 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReadableMap;
+
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.text.TextUtils;
 
 public class RNAnalyticsModule extends ReactContextBaseJavaModule {
-
-  private final ReactApplicationContext reactContext;
-
-  public RNAnalyticsModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
-
-  @Override
-  public String getName() {
-    return "RNAnalytics";
-  }
-
-
+    
+    private final ReactApplicationContext reactContext;
+    
+    public RNAnalyticsModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
+    
+    @Override
+    public String getName() {
+        return "RNAnalytics";
+    }
+    
+    
     @ReactMethod
-  public void start(ReadableMap options) {
-    if(options == null){
-      StatService.start(this.reactContext);
-    } else {
-      StatService.setAppKey(options.getString("appKey"));
-
-          String appChannel=options.getString("appChannel");     
-    if(appChannel != null&&appChannel !="") {
-      StatService.setAppChannel(this.reactContext,appChannel, true);
-    } else {
-      StatService.setAppChannel(this.reactContext,"", false);
+    public void start(ReadableMap options) {
+        if (options == null) {
+            StatService.start(this.reactContext);
+        } else {
+            StatService.setAppKey(options.getString("appKey"));
+            
+            String appChannel = options.getString("appChannel");
+            if (appChannel != null && appChannel != "") {
+                StatService.setAppChannel(this.reactContext, appChannel, true);
+            } else {
+                StatService.setAppChannel(this.reactContext, "", false);
+            }
+            
+            StatService.setDebugOn(options.getBoolean("isDebug"));
+            
+            if (options.getBoolean("enableExceptionLog")) {
+                StatService.setOn(this.reactContext, StatService.EXCEPTION_LOG);
+            }
+            
+            StatService.setSessionTimeOut(options.getInt("sessionTimeOut"));
+            StatService.setLogSenderDelayed(options.getInt("logSenderDelayed"));
+            
+            SendStrategyEnum sendStrategy = SendStrategyEnum.valueOf(options.getString("sendStrategy"));
+            
+            if (sendStrategy != null) {
+                boolean onlyWifi = options.getBoolean("onlyWifi");
+                int logSendInterval = options.getInt("logSendInterval");
+                StatService.setSendLogStrategy(this.reactContext, sendStrategy, logSendInterval, onlyWifi);
+            }
+        }
     }
-
-      StatService.setDebugOn(options.getBoolean("isDebug"));
-
-      if(options.getBoolean("enableExceptionLog")){
-        StatService.setOn(this.reactContext, StatService.EXCEPTION_LOG);
-      }
-
-      StatService.setSessionTimeOut(options.getInt("sessionTimeOut"));
-      StatService.setLogSenderDelayed(options.getInt("logSenderDelayed"));
-
-      SendStrategyEnum sendStrategy = SendStrategyEnum.valueOf(options.getString("sendStrategy"));
-
-      if(sendStrategy != null) {
-        boolean onlyWifi = options.getBoolean("onlyWifi");
-        int logSendInterval = options.getInt("logSendInterval");
-        StatService.setSendLogStrategy(this.reactContext, sendStrategy, logSendInterval, onlyWifi);
-      }
+    
+    @ReactMethod
+    public void onPageStart(String name) {
+        StatService.onPageStart(this.reactContext, name);
     }
-  }
-
-  @ReactMethod
-  public void onPageStart(String name) {
-    StatService.onPageStart(this.reactContext, name);
-  }
-
-  @ReactMethod
-  public void onPageEnd(String name) {
-    StatService.onPageEnd(this.reactContext, name);
-  }
-
-  @ReactMethod
-  public void onPageResume() {
-    StatService.onResume(this.reactContext);
-  }
-
-  @ReactMethod
-  public void onPagePause() {
-    StatService.onPause (this.reactContext);
-  }
-
-  @ReactMethod
-  public void setDebugOn(Boolean isDebug) {
-    StatService.setDebugOn(isDebug);
-  }
-
-  @ReactMethod
-  public void onEvent(String eventId, String label) {
-    StatService.onEvent(this.reactContext, eventId, label);
-  }
-
-  @ReactMethod
-  public void onEventStart(String eventId, String label) {
-    StatService.onEventStart(this.reactContext, eventId, label);
-  }
-
-  @ReactMethod
-  public void onEventEnd(String eventId, String label) {
-    StatService.onEventEnd(this.reactContext, eventId, label);
-  }
-
-  @ReactMethod
-  public void onEventDuration(String eventId, String label, Integer milliseconds) {
-    StatService.onEventDuration(this.reactContext, eventId, label, milliseconds.longValue());
-  }
-
-  @ReactMethod
-  public String getChannel(){
+    
+    @ReactMethod
+    public void onPageEnd(String name) {
+        StatService.onPageEnd(this.reactContext, name);
+    }
+    
+    @ReactMethod
+    public void onPageResume() {
+        StatService.onResume(this.reactContext);
+    }
+    
+    @ReactMethod
+    public void onPagePause() {
+        StatService.onPause(this.reactContext);
+    }
+    
+    @ReactMethod
+    public void setDebugOn(Boolean isDebug) {
+        StatService.setDebugOn(isDebug);
+    }
+    
+    @ReactMethod
+    public void onEvent(String eventId, String label) {
+        StatService.onEvent(this.reactContext, eventId, label);
+    }
+    
+    @ReactMethod
+    public void onEventStart(String eventId, String label) {
+        StatService.onEventStart(this.reactContext, eventId, label);
+    }
+    
+    @ReactMethod
+    public void onEventEnd(String eventId, String label) {
+        StatService.onEventEnd(this.reactContext, eventId, label);
+    }
+    
+    @ReactMethod
+    public void onEventDuration(String eventId, String label, Integer milliseconds) {
+        StatService.onEventDuration(this.reactContext, eventId, label, milliseconds.longValue());
+    }
+    
+    public String getChannelFromAPK() {
         try {
             PackageManager pm = reactContext.getPackageManager();
             ApplicationInfo appInfo = pm.getApplicationInfo(reactContext.getPackageName(), PackageManager.GET_META_DATA);
-            if(appInfo.metaData!=null)
-            return appInfo.metaData.getString("BaiduMobAd_CHANNEL");
-          return null;
+            if (appInfo.metaData != null)
+                return appInfo.metaData.getString("BaiduMobAd_CHANNEL");
+            return null;
         } catch (PackageManager.NameNotFoundException ignored) {
         }
         return null;
-  }
-
-
+    }
+    
+    public void setSpChannel() {
+        SharedPreferences sp = reactContext.
+        getSharedPreferences("BaiduMobAd_CHANNEL", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString("channel", getChannelFromAPK());
+    }
+    
+    public String getSpChannel() {
+        SharedPreferences sp = reactContext.
+        getSharedPreferences("BaiduMobAd_CHANNEL", Context.MODE_PRIVATE);
+        String channel = sp.getString("channel");
+        return channel;
+    }
+    
+    @ReactMethod
+    public String getChannel() {
+        String channel = getSpChannel();
+        if (TextUtils.isEmpty(channel)) {
+            setSpChannel();
+            return getSpChannel();
+        } else {
+            return channel;
+        }
+    }
+    
 }
+
